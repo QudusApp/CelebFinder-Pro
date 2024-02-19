7138f3d99264d47ef5f7701da04150561ed18d94
@Github3636 <qudusnafiu4@gmail.com>
2024-02-19 01:01:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [celebrity, setCelebrity] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(data => {
+            setCelebrity(data);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('There is an error in fetching the celebrity data for you', error);
+        });
+    }, []);
+
+    return(
+        <div className="App">
+            <h1>Welcome to Amatip IT CelebFinder Pro</h1>
+            { loading ? (
+                <p>Loading...</p>
+            ) : (
+                <ul>
+                    {celebrity.map(user => (
+                        <li>
+                            <h2>{user.name}</h2>
+                            <p>{user.email}</p>
+                            <p>{user.website}</p>
+                            <p>{user.phone}</p>
+                            <p>{user.address.city}</p>
+
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
 }
 
 export default App;
